0491a4a75407bf778e1c2dd2b50f6cd964cf5b49
poonam_kushwah <poonam_kushwah@DESKTOP-DJSJOG5>
2024-05-01 20:22:58 +0530

    program

---
+++ src/main/java/com/reverse/ReverseSting.java
@@ -0,0 +1,12 @@
+package com.reverse;
+
+public class ReverseSting {
+
+	public static void main(String[] args) {
+      String a = "poonam";
+      for(int i=a.length()-1; i>=0; i--){
+    	  System.out.print(a.charAt(i));
+      }
+	}
+
+}
